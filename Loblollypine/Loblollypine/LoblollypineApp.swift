753b6cbfe5cae7f0240da914a39b477e5c28cef3
frchase <69663529+frchase@users.noreply.github.com>
2023-04-17 22:12:58 -0400

    adding the project to the github for others to clone

---
+++ Loblollypine/Loblollypine/LoblollypineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoblollypineApp.swift
+//  Loblollypine
+//
+//  Created by Chase Allen on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LoblollypineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
